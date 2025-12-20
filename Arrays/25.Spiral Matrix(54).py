fe3f89ee0d20732f87c84862b13913c60cbc4203
Vraj_Dobariya <158006790+Vraj-Data-Scientist@users.noreply.github.com>
2025-12-19 18:15:15 -0800

    Add files via upload

---
+++ Arrays/25.Spiral Matrix(54).py
@@ -0,0 +1,32 @@
+from typing import List
+
+class Solution:
+    def spiralOrder_optimal(self, matrix: List[List[int]]) -> List[int]:
+        n = len(matrix)
+        m = len(matrix[0])
+        top = 0
+        bottom = n-1
+        left = 0
+        right = m-1
+        ans = []
+        while (top <= bottom and left <= right):
+            for i in range(left, right+1):
+                ans.append(matrix[top][i])
+            top += 1
+            for i in range(top, bottom+1):
+                ans.append(matrix[i][right])
+            right -= 1
+            if (top <= bottom):
+                for i in range(right, left-1, -1):
+                    ans.append(matrix[bottom][i])
+                bottom -= 1
+            if (left <= right):
+                for i in range(bottom, top-1, -1):
+                    ans.append(matrix[i][left])
+                left += 1
+        return ans
+
+
+print(Solution().spiralOrder_optimal([[1,2,3,4],[5,6,7,8],[9,10,11,12]]))
+print(Solution().spiralOrder_optimal([[1,2,3],[4,5,6],[7,8,9]]))
+
