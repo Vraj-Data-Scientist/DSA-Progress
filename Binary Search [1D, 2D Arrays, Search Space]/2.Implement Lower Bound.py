7d822b76fb9faa3c2ea6bd6b7de0a244a5f8c1d5
Vraj_Dobariya <158006790+Vraj-Data-Scientist@users.noreply.github.com>
2026-01-03 13:22:14 -0800

    Add files via upload

---
+++ Binary Search [1D, 2D Arrays, Search Space]/2.Implement Lower Bound.py
@@ -0,0 +1,33 @@
+class Solution:
+    def lowerBound_1(self, arr, target):
+        n = len(arr)
+        low = 0
+        high = n-1
+        while (low <= high):
+            if (low == high and arr[low] >= target):
+                return low
+            mid = (low + high) // 2
+            if (arr[mid] < target):
+                low = mid + 1
+            elif (arr[mid] >= target):
+                high = mid
+        return n
+
+    def lowerBound_2(self, arr, target):
+        n = len(arr)
+        low = 0
+        high = n-1
+        ans = n
+        while (low <= high):
+            mid = (low + high) // 2
+            if (arr[mid] < target):
+                low = mid + 1
+            elif (arr[mid] >= target):
+                ans = mid
+                high = mid - 1
+        return ans
+
+print(Solution().lowerBound_1([2, 3, 7, 10, 11, 11, 25], 9))
+print(Solution().lowerBound_1([2, 3, 7, 10, 11, 11, 25], 100))
+print(Solution().lowerBound_2([2, 3, 7, 10, 11, 11, 25], 9))
+print(Solution().lowerBound_2([2, 3, 7, 10, 11, 11, 25], 100))
