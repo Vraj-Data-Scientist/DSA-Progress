490f5315e22785b9e34193630c3778403b8e9a41
Vraj_Dobariya <158006790+Vraj-Data-Scientist@users.noreply.github.com>
2026-02-11 18:17:13 -0800

    Add files via upload

---
+++ Stack and Queue/12.Prefix to Infix Conversion.py
@@ -0,0 +1,19 @@
+def PrefixToInfix(s):
+    n = len(s)
+    i = n-1
+    st = []
+    while (i >= 0):
+        if ((s[i] >= 'A' and s[i] <= 'Z') or (s[i] >= 'a' and s[i] <= 'z') or (s[i] >= '0' and s[i] <= '9')):
+            st.append(s[i])
+        else:
+            t1 = st[-1]
+            st.pop()
+            t2 = st[-1]
+            st.pop()
+            conv = '(' + t1 + s[i] + t2 + ')'
+            st.append(conv)
+        i -= 1
+    return st[-1]
+
+print(PrefixToInfix('+-*+abcdf'))
+print(PrefixToInfix('/*a+bcd'))
