0111268792a48aa5c3a26a512a6b7153243ba7ac
Vraj_Dobariya <158006790+Vraj-Data-Scientist@users.noreply.github.com>
2026-02-11 17:59:12 -0800

    Add files via upload

---
+++ Stack and Queue/9.Infix to Postfix Conversion.py
@@ -0,0 +1,38 @@
+def prio(c):
+    if c == '^':
+        return 3
+    elif c == '*' or c == '/':
+        return 2
+    elif c == '+' or c == '-':
+        return 1
+    else:
+        return -1
+
+def infixToPostfix(s):
+    n = len(s)
+    i = 0
+    st = []
+    ans = ''
+    while (i < n):
+        if ((s[i] >= 'A' and s[i] <= 'Z') or (s[i] >= 'a' and s[i] <= 'z') or (s[i] >= '0' and s[i] <= '9')):
+            ans = ans + s[i]
+        elif (s[i] == '('):
+            st.append(s[i])
+        elif (s[i] == ')'):
+            while (st and st[-1] != '('):
+                ans += st[-1]
+                st.pop()
+            st.pop()
+        else:
+            while(st and prio(s[i]) <= prio(st[-1])):
+                ans += st[-1]
+                st.pop()
+            st.append(s[i])
+        i += 1
+    while (st):
+        ans += st[-1]
+        st.pop()
+    return ans
+
+print(infixToPostfix('a+b*(c^d-e)'))
+print(infixToPostfix("a*(b+c)/d"))
