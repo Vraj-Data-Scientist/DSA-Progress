6a6df296da34f3305f6e329f097a9c834a1773ba
Vraj_Dobariya <158006790+Vraj-Data-Scientist@users.noreply.github.com>
2026-02-11 18:14:31 -0800

    Add files via upload

---
+++ Stack and Queue/11.Postfix to Infix Conversion.py
@@ -0,0 +1,19 @@
+def PostfixToInfix(s):
+    n = len(s)
+    i = 0
+    st = []
+    while (i < n):
+        if ((s[i] >= 'A' and s[i] <= 'Z') or (s[i] >= 'a' and s[i] <= 'z') or (s[i] >= '0' and s[i] <= '9')):
+            st.append(s[i])
+        else:
+            t1 = st[-1]
+            st.pop()
+            t2 = st[-1]
+            st.pop()
+            conv = '(' + t2 + s[i] + t1 + ')'
+            st.append(conv)
+        i += 1
+    return st[-1]
+
+print(PostfixToInfix('abcd^e-*+'))
+print(PostfixToInfix('abc+*d/'))
