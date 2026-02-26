f8cd6698589cec4793733622bcef7c3e4cc5edd6
Vraj_Dobariya <158006790+Vraj-Data-Scientist@users.noreply.github.com>
2026-02-25 22:12:07 -0800

    Add files via upload

---
+++ Recursion/7.print all subsequences of array with sum k.py
@@ -0,0 +1,26 @@
+def print_all_subsequence(a, i, ds):
+    n = len(a)
+    if (i >= n):
+        print(ds)
+        return
+    ds.append(a[i])
+    print_all_subsequence(a, i+1, ds)
+    ds.remove(a[i])
+    print_all_subsequence(a, i+1, ds)
+
+print(print_all_subsequence([3,1,2], 0, []))
+
+def print_subsequence_for_sum_k(a, i, ds, sum, s):
+    n = len(a)
+    if (i == n):
+        if (s == sum):
+            print(ds)
+        return
+    ds.append(a[i])
+    s += a[i]
+    print_subsequence_for_sum_k(a, i+1, ds, sum, s)
+    ds.remove(a[i])
+    s -= a[i]
+    print_subsequence_for_sum_k(a, i+1, ds, sum, s)
+
+print(print_subsequence_for_sum_k([1,1,3,2,1], 0, [], 2, 0))
