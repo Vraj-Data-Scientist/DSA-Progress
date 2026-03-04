1dc6063eb3e415d28be0b98de13d13e677b4b373
Vraj_Dobariya <158006790+Vraj-Data-Scientist@users.noreply.github.com>
2026-03-03 18:52:05 -0800

    Update 7.print all subsequences of array with sum k.py

--- Recursion/7.print all subsequences of array with sum k.py
+++ Recursion/7.print all subsequences of array with sum k.py
@@ -23,4 +23,20 @@ def print_subsequence_for_sum_k(a, i, ds, sum, s):
     s -= a[i]
     print_subsequence_for_sum_k(a, i+1, ds, sum, s)
 
+def print_subsequence_for_sum_k_part2(a, i, ds, sum):
+    n = len(a)
+    if (i == n):
+        if (sum == 0):
+            print(ds)
+        return
+    ds.append(a[i])
+    print_subsequence_for_sum_k_part2(a, i+1, ds, sum-a[i])
+    ds.pop()
+    print_subsequence_for_sum_k_part2(a, i+1, ds, sum)
+
 print(print_subsequence_for_sum_k([1,1,3,2,1], 0, [], 2, 0))
+print(print_subsequence_for_sum_k_part2([1,1,3,2,1], 0, [], 2))
+print(print_subsequence_for_sum_k([4, -3, 1], 0, [], 2, 0))
+print(print_subsequence_for_sum_k_part2([4, -3, 1], 0, [], 2))
+
+
