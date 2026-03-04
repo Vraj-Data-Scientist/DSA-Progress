99f76e8fe3be6d04d27aa19965128c10f929f6d9
Vraj_Dobariya <158006790+Vraj-Data-Scientist@users.noreply.github.com>
2026-03-03 16:55:29 -0800

    Add files via upload

---
+++ Recursion/11.Combination Sum(39).py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def find_combi(self, ind, arr, ds, ans, target):
+        if (ind == len(arr)):
+            if (target == 0):
+                ans.append(list(ds))
+            return
+        if (arr[ind] <= target):
+            ds.append(arr[ind])
+            self.find_combi(ind, arr, ds, ans, target - arr[ind])
+            ds.pop()
+        self.find_combi(ind+1, arr, ds, ans, target)
+
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        ans = []
+        ds = []
+        self.find_combi(0, candidates, ds, ans, target)
+        return ans
+
+print(Solution().combinationSum([2,3,6,7], 7))
