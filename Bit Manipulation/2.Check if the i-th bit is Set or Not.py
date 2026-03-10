98538e8fb4a11cb1539c8885df5b391c6a60f77e
Vraj_Dobariya <158006790+Vraj-Data-Scientist@users.noreply.github.com>
2026-03-09 21:22:14 -0700

    Update 2.Check if the i-th bit is Set or Not.py

--- Bit Manipulation/2.Check if the i-th bit is Set or Not.py
+++ Bit Manipulation/2.Check if the i-th bit is Set or Not.py
@@ -5,18 +5,24 @@ class Solution:
             return False
         return binary[-(i+1)] == "1"
 
-    def check_ith_bit_optimal_1(self, n, i):
+    def check_ith_bit_1(self, n, i):
         return (n & (1 << i)) != 0
 
-    def check_ith_bit_optimal_2(self, n, i):
+    def check_ith_bit_2(self, n, i):
         return ((n >> i) & 1) != 0
 
+    def set_ith_bit(self, n, i):
+        return n | (1 << i)
+
+    def clear_ith_bit(self, n, i):
+        return n & ~(1 << i)
+
+    def toggle_ith_bit(self, n, i):
+        return n ^ (1 << i)
+
 print(Solution().check_ith_bit_brute(5, 2))
-print(Solution().check_ith_bit_brute(5, 1))
-print(Solution().check_ith_bit_brute(5, 25))
-print(Solution().check_ith_bit_optimal_1(5, 2))
-print(Solution().check_ith_bit_optimal_1(5, 1))
-print(Solution().check_ith_bit_optimal_1(5, 25))
-print(Solution().check_ith_bit_optimal_2(5, 2))
-print(Solution().check_ith_bit_optimal_2(5, 1))
-print(Solution().check_ith_bit_optimal_2(5, 25))
+print(Solution().check_ith_bit_1(5, 2))
+print(Solution().check_ith_bit_2(5, 1))
+print(Solution().set_ith_bit(9, 2))
+print(Solution().clear_ith_bit(13,2))
+print(Solution().toggle_ith_bit(13, 2))
