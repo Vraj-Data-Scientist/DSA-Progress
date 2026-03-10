ca80702c1785c967d59c0f2c474330a3950393e1
Vraj_Dobariya <158006790+Vraj-Data-Scientist@users.noreply.github.com>
2026-03-09 18:21:32 -0700

    Add functions to swap two numbers using different methods

---
+++ Bit Manipulation/1.Swap two numbers.py
@@ -0,0 +1,19 @@
+def swap(a,b):
+    temp = a
+    a = b
+    b = temp
+    return a,b
+
+def swap_python(a,b):
+    a, b = b, a
+    return a, b
+
+def swap_xor(a, b):
+    a = a ^ b
+    b = a ^ b
+    a = a ^ b
+    return a, b
+
+print(swap(5,-6))
+print(swap_python(4, -4))
+print(swap_python(5, -10))
