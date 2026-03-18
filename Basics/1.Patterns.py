8a386dfd7d32bea8b3885dfc1d808585c906b456
Vraj_Dobariya <158006790+Vraj-Data-Scientist@users.noreply.github.com>
2026-03-17 22:10:58 -0700

    Update 1.Patterns.py

--- Basics/1.Patterns.py
+++ Basics/1.Patterns.py
@@ -41,6 +41,53 @@ class Solution:
                 print(N-j+1, end=" ")
             print()
 
+    def pattern8(self, N):
+        for i in range(N):
+            for j in range(0, N-i-1):
+                print(" ", end=" ")
+            for k in range(0, 2*i+1):
+                print("*", end=" ")
+            for l in range(0, N-i-1):
+                print(" ", end=" ")
+            print()
+
+    def pattern9(self, N):
+        for i in range(N):
+            for j in range(0, i):
+                print(" ", end=" ")
+            for k in range(2*N - (2*i+1)):
+                print("*", end=" ")
+            for j in range(0, i):
+                print(" ", end=" ")
+            print()
+
+    def pattern10(self, N):
+        for i in range(N):
+            for j in range(0, N-i-1):
+                print(" ", end=" ")
+            for k in range(0, 2*i+1):
+                print("*", end=" ")
+            for l in range(0, N-i-1):
+                print(" ", end=" ")
+            print()
+        for i in range(N):
+            for j in range(0, i):
+                print(" ", end=" ")
+            for k in range(2*N - (2*i+1)):
+                print("*", end=" ")
+            for j in range(0, i):
+                print(" ", end=" ")
+            print()
+
+    def pattern11(self, N):
+        for i in range(1, 2*N):
+            stars = i
+            if (i > N):
+                stars = 2*N-i
+            for j in range(1, stars+1):
+                print("*", end=" ")
+            print()
+
 
 Solution().pattern1(3)
 Solution().pattern2(5)
@@ -49,3 +96,7 @@ Solution().pattern4(5)
 Solution().pattern5(3)
 Solution().pattern6(5)
 Solution().pattern7(5)
+Solution().pattern8(5)
+Solution().pattern9(5)
+Solution().pattern10(3)
+Solution().pattern11(5)
