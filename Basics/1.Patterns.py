58ce61636c7835052610ee162b87e9555bd6f16b
Vraj_Dobariya <158006790+Vraj-Data-Scientist@users.noreply.github.com>
2026-03-17 23:44:53 -0700

    Add multiple pattern functions to Patterns.py

--- Basics/1.Patterns.py
+++ Basics/1.Patterns.py
@@ -88,6 +88,70 @@ class Solution:
                 print("*", end=" ")
             print()
 
+    def pattern12(self, n):
+        for i in range(0, n):
+            if (i % 2 == 0):
+                start = 1
+            else:
+                start = 0
+            for j in range(0, i+1):
+                print(start, end=" ")
+                start = 1 - start
+            print()
+
+    def pattern13(self, n):
+        space = 2 * (n - 1)
+        for i in range(1, n+1):
+            for j in range(1, i+1):
+                print(j, end=" ")
+            for j in range(1, space+1):
+                print(" ", end=" ")
+            for j in range(i, 0, -1):
+                print(j, end=" ")
+            print()
+            space -= 2
+
+    def pattern14(self, n):
+        num = 1
+        for i in range(0, n):
+            for j in range(0, i+1):
+                print(num, end=" ")
+                num += 1
+            print()
+
+    def pattern15(self, n):
+        for i in range(0, n):
+            for j in range(0, i+1):
+                print(chr(65+j), end=" ")
+            print()
+
+    def pattern16(self, n):
+        for i in range(0, n):
+            for j in range(0, n-i):
+                print(chr(65+j), end=" ")
+            print()
+
+    def pattern17(self, n):
+        for i in range(0, n):
+            for j in range(0, i+1):
+                print(chr(65+i), end=" ")
+            print()
+
+    def pattern18(self, n):
+        for i in range(0, n):
+            for j in range(0, n-i-1):
+                print(" ", end=" ")
+            breakpoint = (2*i+1) // 2
+            ch = ord('A')
+            for j in range(0, 2*i+1):
+                print(chr(ch), end=" ")
+                if (j < breakpoint):
+                    ch += 1
+                else:
+                    ch -= 1
+            for j in range(0, n-i-1):
+                print(" ", end=" ")
+            print()
 
 Solution().pattern1(3)
 Solution().pattern2(5)
@@ -100,3 +164,10 @@ Solution().pattern8(5)
 Solution().pattern9(5)
 Solution().pattern10(3)
 Solution().pattern11(5)
+Solution().pattern12(5)
+Solution().pattern13(4)
+Solution().pattern14(5)
+Solution().pattern15(5)
+Solution().pattern16(5)
+Solution().pattern17(5)
+Solution().pattern18(5)
