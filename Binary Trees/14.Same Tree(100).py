c15f66e77e772975db8c01ae5a6d8517b77a8243
Vraj_Dobariya <158006790+Vraj-Data-Scientist@users.noreply.github.com>
2026-05-07 19:42:13 -0700

    Add files via upload

---
+++ Binary Trees/14.Same Tree(100).py
@@ -0,0 +1,36 @@
+from typing import List
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val = 0, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+node1 = TreeNode(1)
+node2 = TreeNode(2)
+node3 = TreeNode(3)
+node4 = TreeNode(4)
+node5 = TreeNode(5)
+node6 = TreeNode(6)
+node7 = TreeNode(7)
+
+node1.left = node2
+node1.right = node3
+node2.left = node4
+node2.right = node5
+node3.left = node6
+node3.right = node7
+
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if p is None and q is None:
+            return True
+        if p is None or q is None:
+            return False
+        return ((p.val == q.val) and
+                self.isSameTree(p.left, q.left) and
+                self.isSameTree(p.right, q.right))
+
+print(Solution().isSameTree(node1, node1))
+print(Solution().isSameTree(node1, node5))
